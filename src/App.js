d56f81430ef0e9bf430a4281d1d6c778185f7cab
Kostya Kryukaev <kryukaev.kostya@yandex.ru>
2024-09-28 12:12:15 +0300

    added valid-form2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+//import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import styles from './app.module.css';
+import { formProps } from './Components/Props';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const validationSchema = yup.object().shape({
+	email: formProps.fields.email,
+	password: formProps.fields.password,
+	repeatPassword: formProps.fields.repeatPassword,
+});
 
-export default App;
+export const App = () => {
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			repeatPassword: '',
+		},
+		resolver: yupResolver(validationSchema),
+	});
+
+	const emailError = errors.email?.message;
+	const passwordError = errors.password?.message;
+	const repeatPasswordError = errors.repeatPassword?.message;
+
+	const sendFormData = (formData) => {
+		console.log(formData);
+		reset();
+	};
+
+	//const submitButtonRef = useRef(null);
+
+	return (
+		<div className={styles.app}>
+			<h2>Регистрация пользователя</h2>
+			<form className={styles.validForm} onSubmit={handleSubmit(sendFormData)}>
+				<input
+					className={styles.validInput}
+					name="email"
+					type="text"
+					placeholder="email"
+					{...register('email', formProps)}
+				/>
+				{emailError && <div className={styles.errorLabel}>{emailError}</div>}
+				<br />
+				<input
+					className={styles.validInput}
+					name="password"
+					type="password"
+					placeholder="Пароль"
+					{...register('password', formProps)}
+				/>
+				{passwordError && (
+					<div className={styles.errorLabel}>{passwordError}</div>
+				)}
+				<br />
+				<input
+					className={styles.validInput}
+					name="repeatPassword"
+					type="password"
+					placeholder="Повтор пароля"
+					{...register('repeatPassword', formProps)}
+				/>
+				{repeatPasswordError && (
+					<div className={styles.errorLabel}>{repeatPasswordError}</div>
+				)}
+				<br />
+				<button
+					className={styles.validButton}
+					type="submit"
+					disabled={emailError || passwordError || repeatPasswordError}
+					//ref={submitButtonRef}
+				>
+					Зарегистрироваться
+				</button>
+			</form>
+		</div>
+	);
+};
---
+++ src/Components/Props.js
@@ -0,0 +1,39 @@
+import * as yup from 'yup';
+import { cyrillicRegex, emailRegex, passwordRegex } from './Regex';
+
+export const formProps = yup.object().shape({
+	email: yup
+		.string()
+		.required('Необходимо заполнить email-адрес')
+		.test('isEmailValid', 'Неверный формат e-mail', (value) => {
+			return emailRegex.test(value);
+		})
+		.test(
+			'cyrillicValid',
+			'В наименовании почты не должно быть кириллицы',
+			(value) => {
+				return !cyrillicRegex.test(value);
+			},
+		),
+	password: yup
+		.string()
+		.required('Необходимо заполнить пароль')
+		.test(
+			'cyrillicValid',
+			'В названии пароля нельзя использовать кириллицу',
+			(value) => {
+				return !cyrillicRegex.test(value);
+			},
+		)
+		.test(
+			'isPasswordlValid',
+			'Пароль должен содержать минимум 5 символов, включая хотя бы одну заглавную букву и одну цифру, без пробелов',
+			(value) => {
+				return passwordRegex.test(value);
+			},
+		),
+	repeatPassword: yup
+		.string()
+		.required('Необходимо повторно ввести пароль')
+		.oneOf([yup.ref('password'), null], 'Ошибка совместимости значений пароля'),
+});
